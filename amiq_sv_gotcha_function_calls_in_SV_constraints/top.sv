67ee3ea7ae1e44eb874a8a67cf9f86cafa399829
AMIQ Consulting <redactie@amiq.com>
2015-09-24 08:51:12 +0200

    Update top.sv

--- amiq_sv_gotcha_function_calls_in_SV_constraints/top.sv
+++ amiq_sv_gotcha_function_calls_in_SV_constraints/top.sv
@@ -1,3 +1,24 @@
+/******************************************************************************
+* (C) Copyright 2015 AMIQ Consulting
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* MODULE:      BLOG
+* PROJECT:     Gotcha: Function Calls in SystemVerilog Constraints
+* Description: This is a code snippet from the Blog article mentioned on PROJECT
+* Link:        http://www.amiq.com/consulting/2015/03/12/gotcha-function-calls-in-systemverilog-constraints/
+*******************************************************************************/
+
 module top;
    initial begin
       automatic constraint_container cc_inst = new();
