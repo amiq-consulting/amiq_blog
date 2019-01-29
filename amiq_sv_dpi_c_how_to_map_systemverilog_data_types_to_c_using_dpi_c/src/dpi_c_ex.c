72a173606bdb6b08104fe1eed6ac51b7b16d2249
AMIQ Consulting <office@amiq.com>
2019-01-29 07:55:51 +0200

    Update dpi_c_ex.c
    * updated article link
    * type pointers are now next to the type
    * added const to all inputs

--- amiq_sv_dpi_c_how_to_map_systemverilog_data_types_to_c_using_dpi_c/src/dpi_c_ex.c
+++ amiq_sv_dpi_c_how_to_map_systemverilog_data_types_to_c_using_dpi_c/src/dpi_c_ex.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* (C) Copyright 2018 AMIQ Consulting
+* (C) Copyright 2019 AMIQ Consulting
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 * MODULE:      BLOG
 * PROJECT:     How to Map SystemVerilog Data Types to C, Using DPI-C
 * Description: This is a code snippet from the Blog article mentioned on PROJECT
-* Link:
+* Link:        https://www.amiq.com/consulting/2019/01/30/how-to-map-systemverilog-data-types-to-c-using-dpi-c/
 *******************************************************************************/
 
 #define _GNU_SOURCE
@@ -32,59 +32,59 @@
 #ifndef __dpi_c_ex_c
 #define __dpi_c_ex_c
 
-void compute_byte(const char i_value, char *result) {
-	log_info("dpi_c.compute_byte(): received value %d", (int ) (i_value));
+void compute_byte(const char i_value, char* result) {
+	log_info("dpi_c.compute_byte(): received value %d", (int) (i_value));
 	*result = transform_char(i_value);
-	log_info("dpi_c.compute_byte(): return value %d", (int ) (*result));
+	log_info("dpi_c.compute_byte(): return value %d", (int) (*result));
 }
 
 char get_byte(const char i_value) {
-	log_info("dpi_c.get_byte(): received %d", (int ) (i_value));
+	log_info("dpi_c.get_byte(): received %d", (int) (i_value));
 	char result = transform_char(i_value);
-	log_info("dpi_c.get_byte(): return %d", (int ) (result));
+	log_info("dpi_c.get_byte(): return %d", (int) (result));
 	return result;
 }
 
-void compute_shortint(const short int i_value, short int *result) {
-	log_info("dpi_c.compute_shortint(): received %d", (int )(i_value));
+void compute_shortint(const short int i_value, short int* result) {
+	log_info("dpi_c.compute_shortint(): received %d", (int)(i_value));
 	*result = transform_short_int(i_value);
-	log_info("dpi_c.compute_shortint(): return %d", (int )(*result));
+	log_info("dpi_c.compute_shortint(): return %d", (int)(*result));
 }
 
 short int get_shortint(const short int i_value) {
-	log_info("dpi_c.get_shortint(): received %d", (int )(i_value));
+	log_info("dpi_c.get_shortint(): received %d", (int)(i_value));
 	short int result = transform_short_int(i_value);
-	log_info("dpi_c.get_shortint(): return %d", (int )(result));
+	log_info("dpi_c.get_shortint(): return %d", (int)(result));
 	return result;
 }
 
-void compute_int(const int i_value, int *result) {
-	log_info("dpi_c.compute_int(): received %d", (int )(i_value));
+void compute_int(const int i_value, int* result) {
+	log_info("dpi_c.compute_int(): received %d", (int)(i_value));
 	*result = transform_int(i_value);
-	log_info("dpi_c.compute_int(): return %d", (int )(*result));
+	log_info("dpi_c.compute_int(): return %d", (int)(*result));
 }
 
 int get_int(const int i_value) {
-	log_info("dpi_c.get_int(): received %d", (int )(i_value));
+	log_info("dpi_c.get_int(): received %d", (int)(i_value));
 	int result = transform_int(i_value);
-	log_info("dpi_c.get_int(): return %d", (int )(result));
+	log_info("dpi_c.get_int(): return %d", (int)(result));
 	return result;
 }
 
-void compute_longint(const long int i_value, long int *result) {
-	log_info("dpi_c.compute_longint(): received %d", (int )(i_value));
+void compute_longint(const long int i_value, long int* result) {
+	log_info("dpi_c.compute_longint(): received %d", (int)(i_value));
 	*result = transform_long_int(i_value);
-	log_info("dpi_c.compute_longint(): return %d", (int )(*result));
+	log_info("dpi_c.compute_longint(): return %d", (int)(*result));
 }
 
 long int get_longint(const long int i_value) {
-	log_info("dpi_c.get_longint(): received %d", (int )(i_value));
+	log_info("dpi_c.get_longint(): received %d", (int)(i_value));
 	long int result = transform_long_int(i_value);
-	log_info("dpi_c.get_longint(): return %d", (int )(result));
+	log_info("dpi_c.get_longint(): return %d", (int)(result));
 	return result;
 }
 
-void compute_real(const double i_value, double *result) {
+void compute_real(const double i_value, double* result) {
 	log_info("dpi_c.compute_real(): received %f", i_value);
 	*result = transform_double(i_value);
 	log_info("dpi_c.compute_real(): return %f", *result);
@@ -97,23 +97,23 @@ double get_real(const double i_value) {
 	return result;
 }
 
-void compute_string(const char *i_value, char **result) {
+void compute_string(const char* i_value, char** result) {
 	log_info("dpi_c.compute_string(): received %s", i_value);
 	*result = "DEAF_BEAF_DRINKS_COFFEE";
 	log_info("dpi_c.compute_string(): return %s", *result);
 }
 
-char * get_string(const char *i_value) {
+char* get_string(const char* i_value) {
 	log_info("dpi_c.get_string(): received %s", i_value);
-	char *result = "DEAF_BEAF_DRINKS_COFFEE";
+	char* result = "DEAF_BEAF_DRINKS_COFFEE";
 	log_info("dpi_c.get_string(): return %s", result);
 	return result;
 }
 
 void compute_string_array(const svOpenArrayHandle i_value,
 		const svOpenArrayHandle result) {
-	char ** i_val = svGetArrayPtr(i_value);
-	char ** o_val = svGetArrayPtr(result);
+	char** i_val = svGetArrayPtr(i_value);
+	char** o_val = svGetArrayPtr(result);
 	log_info("dpi_c.compute_string_array(): inputs {%s, %s, %s}", i_val[0],
 			i_val[1], i_val[2]);
 	o_val[0] = "DEAF_BEAF";
@@ -123,13 +123,13 @@ void compute_string_array(const svOpenArrayHandle i_value,
 			o_val[0], o_val[1], o_val[2]);
 }
 
-void compute_bit(svBit i_value, svBit *result) {
+void compute_bit(const svBit i_value, svBit* result) {
 	log_info("dpi_c.compute_bit(): input %u", i_value);
 	*result = transform_svBit(i_value);
 	log_info("dpi_c.compute_bit(): result %u", *result);
 }
 
-svBit get_bit(svBit i_value) {
+svBit get_bit(const svBit i_value) {
 	svBit result;
 	log_info("dpi_c.get_bit(): input %u", i_value);
 	result = transform_svBit(i_value);
@@ -137,13 +137,13 @@ svBit get_bit(svBit i_value) {
 	return result;
 }
 
-void compute_bit_vector(const svBitVecVal *i_value, svBitVecVal *result) {
+void compute_bit_vector(const svBitVecVal* i_value, svBitVecVal* result) {
 	log_info("dpi_c.compute_bit_vector(): input %u", *i_value);
 	*result = transform_svBitVecVal(i_value);
 	log_info("dpi_c.compute_bit_vector(): result %u", *result);
 }
 
-svBitVecVal get_bit_vector(const svBitVecVal *i_value) {
+svBitVecVal get_bit_vector(const svBitVecVal* i_value) {
 	svBitVecVal result;
 	log_info("dpi_c.get_bit_vector(): input %u", *i_value);
 	result = transform_svBitVecVal(i_value);
@@ -151,14 +151,14 @@ svBitVecVal get_bit_vector(const svBitVecVal *i_value) {
 	return result;
 }
 
-void compute_logic(svLogic i_value, svLogic *result) {
+void compute_logic(const svLogic i_value, svLogic* result) {
 	log_info("dpi_c.compute_logic(): integer value:%d, input %s", (int) i_value, svLogic2String(i_value));
 	*result = transform_svLogic(i_value);
 	log_info("dpi_c.compute_logic(): result %s <- %s", svLogic2String(*result),
 			svLogic2String(i_value));
 }
 
-svLogic get_logic(svLogic i_value) {
+svLogic get_logic(const svLogic i_value) {
 	svLogic result = sv_0;
 	log_info("dpi_c.get_logic(): input %s", svLogic2String(i_value));
 	result = transform_svLogic(i_value);
@@ -167,7 +167,7 @@ svLogic get_logic(svLogic i_value) {
 	return result;
 }
 
-void compute_logic_vector(svLogicVecVal *i_value, svLogicVecVal *result,
+void compute_logic_vector(const svLogicVecVal* i_value, svLogicVecVal* result,
 		int asize) {
 	log_info("dpi_c.compute_logic_vector(): input %s",
 			svLogicVecVal2String(i_value, asize));
@@ -181,8 +181,8 @@ void compute_logic_vector(svLogicVecVal *i_value, svLogicVecVal *result,
 			svLogicVecVal2String(result, asize));
 }
 
-svLogicVecVal * get_logic_vector(svLogicVecVal *i_value, int asize) {
-	svLogicVecVal *result = malloc(sizeof(svLogicVecVal));
+svLogicVecVal* get_logic_vector(const svLogicVecVal* i_value, int asize) {
+	svLogicVecVal* result = malloc(sizeof(svLogicVecVal));
 	log_info("dpi_c.get_logic_vector(): input %s",
 			svLogicVecVal2String(i_value, asize));
 	int i;
@@ -196,13 +196,13 @@ svLogicVecVal * get_logic_vector(svLogicVecVal *i_value, int asize) {
 	return result;
 }
 
-void compute_reg(svLogic i_value, svLogic *result) {
+void compute_reg(const svLogic i_value, svLogic* result) {
 	log_info("dpi_c.compute_reg(): input %u", i_value);
 	*result = transform_svLogic(i_value);
 	log_info("dpi_c.compute_reg(): result %u", *result);
 }
 
-svLogic get_reg(svLogic i_value) {
+svLogic get_reg(const svLogic i_value) {
 	svLogic result = sv_0;
 	log_info("dpi_c.get_reg(): input %u", i_value);
 	result = transform_svLogic(i_value);
@@ -210,7 +210,7 @@ svLogic get_reg(svLogic i_value) {
 	return result;
 }
 
-void compute_reg_vector(svLogicVecVal *i_value, svLogicVecVal *result,
+void compute_reg_vector(const svLogicVecVal* i_value, svLogicVecVal* result,
 		int asize) {
 	log_info("dpi_c.compute_reg_vector(): input %s",
 			svLogicVecVal2String(i_value, asize));
@@ -224,8 +224,8 @@ void compute_reg_vector(svLogicVecVal *i_value, svLogicVecVal *result,
 			svLogicVecVal2String(result, asize));
 }
 
-svLogicVecVal * get_reg_vector(svLogicVecVal *i_value, int asize) {
-	svLogicVecVal *result = malloc(sizeof(svLogicVecVal));
+svLogicVecVal* get_reg_vector(const svLogicVecVal* i_value, int asize) {
+	svLogicVecVal* result = malloc(sizeof(svLogicVecVal));
 	log_info("dpi_c.get_reg_vector(): input %s",
 			svLogicVecVal2String(i_value, asize));
 	int i;
@@ -242,14 +242,14 @@ svLogicVecVal * get_reg_vector(svLogicVecVal *i_value, int asize) {
 void compute_chandle(void** result) {
 	int (*pcp)();
 	pcp = print_chandle;
-	*result = (void *) pcp;
+	*result = (void*) pcp;
 	log_info("dpi_c.compute_chandle() %p ", result);
 }
 
 void** get_chandle() {
 	int (*pcp)();
 	pcp = print_chandle;
-	void** result = (void *) pcp;
+	void** result = (void*) pcp;
 	log_info("dpi_c.get_chandle() %p ", result);
 	return result;
 }
@@ -264,29 +264,28 @@ void call_chandle(const void* i_value, int* o_value) {
 
 void compute_unsized_int_array(const svOpenArrayHandle i_value,	svOpenArrayHandle result) {
 	int count = svSize(i_value, 1);
-	memcpy((int *) svGetArrayPtr(result), (int *) svGetArrayPtr(i_value),
+	memcpy((int*) svGetArrayPtr(result), (int*) svGetArrayPtr(i_value),
 			svSizeOfArray(i_value));
 
 	while (count--) {
-		int* tmp = (int *) svGetArrElemPtr1(result, count);
+		int* tmp = (int*) svGetArrElemPtr1(result, count);
 		*tmp += 3;
 	}
 }
 
 void compute_unsized_byte_array(const svOpenArrayHandle i_value, svOpenArrayHandle result) {
 	int count = svSize(i_value, 1);
-	memcpy((char *) svGetArrayPtr(result), (char *) svGetArrayPtr(i_value),
+	memcpy((char*) svGetArrayPtr(result), (char*) svGetArrayPtr(i_value),
 			svSizeOfArray(i_value));
 
 	while (count--) {
-		char* tmp = (char *) svGetArrElemPtr1(result, count);
+		char* tmp = (char*) svGetArrElemPtr1(result, count);
 		*tmp += 3;
 	}
 };
 
-void compute_struct(const dpi_c_ex_s *i_value, dpi_c_ex_s *output) {
+void compute_struct(const dpi_c_ex_s* i_value, dpi_c_ex_s* output) {
 	*output = transform_struct(i_value);
 }
 
 #endif
-
